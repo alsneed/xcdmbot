eb9a759cf8c72341cc6d7e6b5710ed837a18c7cd
billwilsons <40507539+billwilsons@users.noreply.github.com>
2020-02-08 18:12:57 -0500

    baneposting
    bane?

--- src/bot.py
+++ src/bot.py
@@ -10,6 +10,18 @@ async def on_ready():
     print('I live. Logged in as')
     print(bot.user.name)
     print(bot.user.id)
+    
+@bot.event
+async def on_message(msg):
+    if msg.author == bot.user:
+        return
+    
+    #banepost
+    str_list = msg.lower().split(' ')
+    for idx, string in enumerate(str_list, start = 1):
+        if string == 'big' and idx != len(str_list):
+           await msg.channel.send('For you')
+           break
 
 bot.load_extension('commands.yt')
 
